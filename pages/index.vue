c78a3d6bcdae044aa5bb81fa0e64a4d783be5274
Jang Haemin <io@jhaemin.com>
2019-05-31 10:23:01 +0900

    Update home

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,44 @@
 <template>
-  <div id="home">Payw</div>
+  <div id="home">
+    <div class="container">
+      <img src="~/assets/images/icon_transparent.png" alt="">
+      <h1 class="title">Payw</h1>
+      <p class="manifesto">Pay as you want</p>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#home {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  img {
+    width: 3rem;
+    height: 3rem;
+  }
+
+  .title {
+    font-weight: 800;
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  .manifesto {
+    font-size: 1rem;
+    color: #7a7a7a;
+    margin: 0;
+    margin-top: 0.3rem;
+  }
+}
+</style>
