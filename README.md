97089969b2573e5bae2fc3a8f1a7c7c5941601b6
jhaemin <io@jhaemin.com>
2020-04-24 23:46:01 +0900

    Redesign website

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# payw.org
-PAYW website
+<h1 align="center">payw.org</h1>
+
+<h3 align="center">Welcome to PAYW website</h3>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,60 @@
-import React from 'react'
-import './Home.scss'
-import logo from 'assets/graphics/icons/v2-aligned.svg'
+import './style.scss'
 
-export default function() {
+import React, { useEffect, useState } from 'react'
+
+import Logo from 'assets/graphics/icons/v2-aligned.svg'
+import classnames from 'classnames'
+
+export default function () {
+  const [isFontsLoaded, setIsFontsLoaded] = useState(false)
+
+  useEffect(() => {
+    ;(document as any).fonts.ready.then(() => {
+      setIsFontsLoaded(true)
+    })
+  }, [])
+
   return (
-    <div id="home">
+    <div
+      id="home"
+      className={classnames({
+        'is-fonts-loaded': isFontsLoaded,
+      })}
+    >
       <div className="hero">
-        <img src={logo} alt="payw logo" className="logo" />
-        <h1 className="title">PAYW</h1>
-        <p className="manifesto">
-          We
-          <span className="heart" role="img" aria-label="red-heart">
-            ❤️
-          </span>
-          Open Source
-        </p>
-        <button className="join">
-          <a
-            href="https://github.com/paywteam/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-          </a>
-          Join us at GitHub
-        </button>
+        <img src={Logo} alt="PAYW Logo" className="logo" />
+
+        <section className="header">
+          <h1 className="organization-name">
+            <span>PAYW</span>
+          </h1>
+
+          <h2 className="organization-full-name">
+            <span className="splash">Pay</span>{' '}
+            <span className="splash">As</span>{' '}
+            <span className="splash">You</span>{' '}
+            <span className="splash">Want</span>
+          </h2>
+        </section>
+
+        <section className="manifesto">
+          <p className="paragraph">
+            Our mission is to give people a great experience through our great
+            software which always are and will be open to you. You don’t have to
+            pay anything.
+          </p>
+        </section>
+
+        <a
+          href="https://github.com/paywteam"
+          className="visit-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button tabIndex={-1} className="visit-github-btn">
+            Visit GitHub
+          </button>
+        </a>
       </div>
     </div>
   )
