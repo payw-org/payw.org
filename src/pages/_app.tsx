7b82f1012a347ff338143e200eeb7441f3335f5c
jhaemin <io@jhaemin.com>
2020-10-06 23:27:26 +0900

    feat: annex head information

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
+import PageInfo from '@/components/PageInfo'
 import '@/styles/globals.scss'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <PageInfo title="PAYW" description="Open Source Group" />
+      <Head>
+        <meta name="author" content="jhaemin" />
+        <meta
+          name="keywords"
+          content="Open Source, 오픈소스, PAYW, 페이더블유, pay as you want"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
